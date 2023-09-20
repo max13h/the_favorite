b2dc06c076a4807cb58fb80aa80f9877529813ec
max13h <contact@max13h.fr>
2023-09-20 16:56:07 +0200

    starting implementation of 'add a family' + every guard

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_17_133203) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_100001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -108,7 +108,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_17_133203) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "couple_id", null: false
+    t.bigint "couple_id"
     t.index ["couple_id"], name: "index_users_on_couple_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
